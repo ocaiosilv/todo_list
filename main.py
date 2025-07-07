9f3fe3bd34dedd824898cd5789bf7391cb82678e
testador <umasenhafacil1/gmail.com>
2025-07-07 18:36:08 -0300

    feat: CLI menu and Task class skeleton done

--- main.py
+++ main.py
@@ -1,10 +1,53 @@
 """from tarefa import """
-
-
-
+def menu(tasks_list): ##The basic menu
+    print("Your's to do List")
+    base = "_" * (len(max(tasks_list, key=len)))
+    print(base)
+    number = 1
+    for i in tasks_list:
+        print(number,"° - ",i," -> ", tasks_list[i])
+        number +=1
+    print(base)
+    print("Please choose an option:")
+    print("1 - Add a new Task")
+    print("2 - Update Task status")
+    print("3 - Remove a Task")
+    print("4 - Edit a Task")
+    while True:
+        a = input(":  ")
+        try:
+            intA = int(a)
+            if intA in [1,2,3,4]:
+                break
+            else:
+               print("Please, type the number of one of the options")
+        except ValueError: ## type error
+            print("Please, type an number")
+    return a
 
 def main():
-    pass
+    tasksDict = {}
+    print("Your's to do List")
+    print("_________________________________________________________")
+    print("                          empty                          ")
+    print("_________________________________________________________")
+    
+    print("Please start with adding a task:")
+    tasksDict.update({str(input()):" Work in progress"})
+    print("Very Well")
+
+    while True:
+        res = menu(tasksDict)
+        match res:
+            case 1:
+                pass
+            case 2:
+                pass
+            case 3:
+                pass
+            case 4:
+                pass
+            
 
 if __name__ == "__main__":
     main()
---
+++ task.py
@@ -0,0 +1,11 @@
+class task():
+    def __init__(self):
+        pass
+    def changeStatus():
+        pass
+    def editTask():
+        pass
+    def dateAdded():
+        pass
+    def expireDate():
+        pass
